e171e4b8cbf18ed28be02d76424cd15d9fa514ac
eastor112 <emerar.mct@gmail.com>
2023-12-25 16:23:45 -0500

    [fix] fixed package name on proto file

--- gen/patients/v1/patientsv1connect/patient.connect.go
+++ gen/patients/v1/patientsv1connect/patient.connect.go
@@ -2,7 +2,7 @@
 //
 // Source: patients/v1/patient.proto
 
-package v1connect
+package patientsv1connect
 
 import (
 	connect "connectrpc.com/connect"
@@ -22,7 +22,7 @@ const _ = connect.IsAtLeastVersion1_13_0
 
 const (
 	// PatientServiceName is the fully-qualified name of the PatientService service.
-	PatientServiceName = "patients.PatientService"
+	PatientServiceName = "patients.v1.PatientService"
 )
 
 // These constants are the fully-qualified names of the RPCs defined in this package. They're
@@ -35,7 +35,7 @@ const (
 const (
 	// PatientServiceGetPatientByIDProcedure is the fully-qualified name of the PatientService's
 	// GetPatientByID RPC.
-	PatientServiceGetPatientByIDProcedure = "/patients.PatientService/GetPatientByID"
+	PatientServiceGetPatientByIDProcedure = "/patients.v1.PatientService/GetPatientByID"
 )
 
 // These variables are the protoreflect.Descriptor objects for the RPCs defined in this package.
@@ -44,15 +44,15 @@ var (
 	patientServiceGetPatientByIDMethodDescriptor = patientServiceServiceDescriptor.Methods().ByName("GetPatientByID")
 )
 
-// PatientServiceClient is a client for the patients.PatientService service.
+// PatientServiceClient is a client for the patients.v1.PatientService service.
 type PatientServiceClient interface {
 	GetPatientByID(context.Context, *connect.Request[v1.PatientRequest]) (*connect.Response[v1.Patient], error)
 }
 
-// NewPatientServiceClient constructs a client for the patients.PatientService service. By default,
-// it uses the Connect protocol with the binary Protobuf Codec, asks for gzipped responses, and
-// sends uncompressed requests. To use the gRPC or gRPC-Web protocols, supply the connect.WithGRPC()
-// or connect.WithGRPCWeb() options.
+// NewPatientServiceClient constructs a client for the patients.v1.PatientService service. By
+// default, it uses the Connect protocol with the binary Protobuf Codec, asks for gzipped responses,
+// and sends uncompressed requests. To use the gRPC or gRPC-Web protocols, supply the
+// connect.WithGRPC() or connect.WithGRPCWeb() options.
 //
 // The URL supplied here should be the base URL for the Connect or gRPC server (for example,
 // http://api.acme.com or https://acme.com/grpc).
@@ -73,12 +73,12 @@ type patientServiceClient struct {
 	getPatientByID *connect.Client[v1.PatientRequest, v1.Patient]
 }
 
-// GetPatientByID calls patients.PatientService.GetPatientByID.
+// GetPatientByID calls patients.v1.PatientService.GetPatientByID.
 func (c *patientServiceClient) GetPatientByID(ctx context.Context, req *connect.Request[v1.PatientRequest]) (*connect.Response[v1.Patient], error) {
 	return c.getPatientByID.CallUnary(ctx, req)
 }
 
-// PatientServiceHandler is an implementation of the patients.PatientService service.
+// PatientServiceHandler is an implementation of the patients.v1.PatientService service.
 type PatientServiceHandler interface {
 	GetPatientByID(context.Context, *connect.Request[v1.PatientRequest]) (*connect.Response[v1.Patient], error)
 }
@@ -95,7 +95,7 @@ func NewPatientServiceHandler(svc PatientServiceHandler, opts ...connect.Handler
 		connect.WithSchema(patientServiceGetPatientByIDMethodDescriptor),
 		connect.WithHandlerOptions(opts...),
 	)
-	return "/patients.PatientService/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	return "/patients.v1.PatientService/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		switch r.URL.Path {
 		case PatientServiceGetPatientByIDProcedure:
 			patientServiceGetPatientByIDHandler.ServeHTTP(w, r)
@@ -109,5 +109,5 @@ func NewPatientServiceHandler(svc PatientServiceHandler, opts ...connect.Handler
 type UnimplementedPatientServiceHandler struct{}
 
 func (UnimplementedPatientServiceHandler) GetPatientByID(context.Context, *connect.Request[v1.PatientRequest]) (*connect.Response[v1.Patient], error) {
-	return nil, connect.NewError(connect.CodeUnimplemented, errors.New("patients.PatientService.GetPatientByID is not implemented"))
+	return nil, connect.NewError(connect.CodeUnimplemented, errors.New("patients.v1.PatientService.GetPatientByID is not implemented"))
 }
